fb963f9c4a78980a12905b4a3f86599547e158bc
Mikel-Jon West <dwmyke@gmail.com>
2020-01-03 08:30:43 -0800

    Initial commit from gatsby: (https://github.com/kendallstrautman/brevifolia-gatsby-tinacms.git)

---
+++ gatsby-config.js
@@ -0,0 +1,80 @@
+const config = require("./config.json")
+
+module.exports = {
+  //this makes the site config available to forestry cms
+  siteMetadata: {
+    title: config.title,
+    description: config.description,
+    repoUrl: config.repository_url,
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
+    "gatsby-transformer-json",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/content/posts`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "data",
+        path: `${__dirname}/content/data`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "images",
+        path: `${__dirname}/content/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-sharp", 
+      options: {
+        defaultQuality: 75
+      }
+    },
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          "gatsby-remark-normalize-paths",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1000,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-tinacms-json',
+    {
+      resolve: 'gatsby-plugin-tinacms',
+      options: {
+        plugins: [
+          "gatsby-tinacms-git",
+          "gatsby-tinacms-remark",
+        ],
+        sidebar: {
+          hidden: process.env.NODE_ENV === "production"
+        },
+      },
+    },
+  ],
+}
